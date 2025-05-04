181b0c31adc634f40045889d85077a79313d3627
mAru <maru1015928@gmail.com>
2025-05-04 18:11:28 +0900

    Update

--- env-exec-shell-program.bat
+++ env-exec-shell-program.bat
@@ -8,7 +8,7 @@ if %ERRORLEVEL% equ 0 (
     echo [%eec_deleter%] は既に実行中です。
 ) else (
     echo [%eec_deleter%] を起動します…
-    start "eec_deleter" "%eec_deleter%"
+    start "%eec_deleter%" "%eec_deleter%"
 )
 
 REM eec run --config-file "%USERPROFILE%\env-exec-config.toml" --program powershell --  "-NoExit" "-Command" "Set-ExecutionPolicy RemoteSigned -Scope Process; Set-Location -Path 'D:\win\program\'"
