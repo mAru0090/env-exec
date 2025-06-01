824043e9a645f6c1d8831ed8839f26a7e8d57b2b
mAru <maru1015928@gmail.com>
2025-06-02 07:08:17 +0900

    Update

--- env-exec-scripts/env-exec-add-tags.bat
+++ env-exec-scripts/env-exec-add-tags.bat
@@ -1,10 +1,10 @@
 @echo off
 chcp 65001
 echo タグを設定します...
-eec-tag --tag-name powershell00 --config-file "%USERPROFILE%\env-exec-configs\env-exec-config.toml" --program "powershell"  -- "-NoExit" "-Command" "Set-ExecutionPolicy RemoteSigned -Scope Process; checkitems %USERPROFILE%\env-exec-configs\checkitems.csv"
-eec-tag --tag-name powershell01 --config-file "%USERPROFILE%\env-exec-configs\env-exec-config.toml" --program "powershell"  -- "-NoExit" "-Command" "Set-ExecutionPolicy RemoteSigned -Scope Process; checkitems %USERPROFILE%\env-exec-configs\checkitems.csv; Set-Location -Path 'D:\win\program\'"
-eec-tag --tag-name cmd00 --config-file "%USERPROFILE%\env-exec-configs\env-exec-config.toml"  --program cmd -- "/K checkitems %USERPROFILE%\env-exec-configs\checkitems.csv"
-eec-tag --tag-name cmd01 --config-file "%USERPROFILE%\env-exec-configs\env-exec-config.toml " --program cmd -- "/K checkitems %USERPROFILE%\env-exec-configs\checkitems.csv & cd /d D:\win\program\"
+eec tag add --name powershell00 --config-file "%USERPROFILE%\env-exec-configs\env-exec-config.toml" --program "powershell"  -- "-NoExit" "-Command" "Set-ExecutionPolicy RemoteSigned -Scope Process; checkitems %USERPROFILE%\env-exec-configs\checkitems.csv"
+eec tag add --name powershell01 --config-file "%USERPROFILE%\env-exec-configs\env-exec-config.toml" --program "powershell"  -- "-NoExit" "-Command" "Set-ExecutionPolicy RemoteSigned -Scope Process; checkitems %USERPROFILE%\env-exec-configs\checkitems.csv; Set-Location -Path 'D:\win\program\'"
+eec tag add --name cmd00 --config-file "%USERPROFILE%\env-exec-configs\env-exec-config.toml"  --program cmd -- "/K checkitems %USERPROFILE%\env-exec-configs\checkitems.csv"
+eec tag add --name cmd01 --config-file "%USERPROFILE%\env-exec-configs\env-exec-config.toml " --program cmd -- "/K checkitems %USERPROFILE%\env-exec-configs\checkitems.csv & cd /d D:\win\program\"
 
 
 echo タグの設定が終了しました
